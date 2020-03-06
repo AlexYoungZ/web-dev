196a599a657782e12178183643d6a25aaad4feb7
AlexYoungZ <zhangsiy2015@163.com>
2020-03-06 10:16:29 +0800

    more Express and movie app

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // embedded js
 
 app.get("/", function(req, res){
     res.render("home");
@@ -23,6 +23,6 @@ app.get("/posts", function(req, res){
     res.render("posts", {posts: posts});
 })
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
    console.log("Server is listening!!!"); 
 });
